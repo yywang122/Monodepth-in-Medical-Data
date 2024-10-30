905985fa4948d08a09ba170e40f84789110285dd
yywang <144195750+yywang122@users.noreply.github.com>
2024-10-30 11:24:11 +0800

    Update camera_cali.py

--- camera_cali.py
+++ camera_cali.py
@@ -1,29 +1,16 @@
 import cv2
 import numpy as np
 import glob
-'''camera = cv2.VideoCapture(0)
-i = 1
-while i <15:
-    _, frame = camera.read()
-    cv2.imshow('frame', frame)
-    if ord('q'):
-        cv2.imwrite("C:/Users/User/Desktop/camera/"+"IMG_40%s"%str(100+i)+'.jpg', frame, [int(cv2.IMWRITE_PNG_COMPRESSION), 0]) 
-        
-        i += 1
-    elif 0xFF == 27: # 按ESC键退出
-        break
-cv2.destroyAllWindows()
-'''
 
 
 dir_li=['0.4']
 
 name_li=['05','1','2','3']
 for dir in dir_li:
-    print('=====================================================')
+    
     print(dir)
     for name in  name_li:
-        print('~~~~focal~~~')
+        print('focal')
         print(name)
         objp = np.zeros((5 * 5, 3), np.float32)
         objp[:, :2] = np.mgrid[0:5, 0:5].T.reshape(-1, 2)  
